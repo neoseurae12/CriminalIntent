02bcbf91c18f9c88e28c72c56289479b01f3d579
Wonhui Roh <rwhui012@gmail.com>
2024-06-02 16:59:12 +0900

    chore: add Fragment library to Gradle build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.fragment.ktx)  // Fragment library
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
