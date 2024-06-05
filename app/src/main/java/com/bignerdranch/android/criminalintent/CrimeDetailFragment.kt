bf5552a441a242d07c835f86b3f353a775779237
Wonhui Roh <rwhui012@gmail.com>
2024-06-05 14:29:29 +0900

    Create the class and set its view to be the layout that I defined

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment: Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
