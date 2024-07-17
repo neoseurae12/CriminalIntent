d34fa031a92b35c1e5f7db4ebd0ac9379176b20d
Wonhui Roh <rwhui012@gmail.com>
2024-07-17 21:12:37 +0900

    Create `CrimeListAdapter` class
    - override three functions:
      1) onCreateViewHolder(...),
      2) onBindViewHolder(...),
      3) getItemCount()
    - 오타 수정: data -> date

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
@@ -9,5 +11,22 @@ class CrimeHolder(
 
 }
 
-class CrimeListAdapter {
+class CrimeListAdapter(
+    private val crimes: List<Crime>
+) : RecyclerView.Adapter<CrimeHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+        val crime = crimes[position]
+        holder.apply {
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+        }
+    }
+
+    override fun getItemCount(): Int = crimes.size
 }
