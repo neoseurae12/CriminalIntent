d7aa81a2e63515ea4a76518a0488e6593febe8bb
Wonhui Roh <rwhui012@gmail.com>
2024-07-17 18:38:32 +0900

    Hook up the view to the fragment in `CrimeListFragment.kt`
    - inflate & bind
    - null out the binding in `onDestroyView()`

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -2,12 +2,22 @@ package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeListBinding
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+
+    private var _binding: FragmentCrimeListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
@@ -15,5 +25,19 @@ class CrimeListFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
