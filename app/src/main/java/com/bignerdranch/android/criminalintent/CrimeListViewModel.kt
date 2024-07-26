c7872f2c0acbc41647f601b577c1d6b9edb1ceab
Wonhui Roh <rwhui012@gmail.com>
2024-07-26 16:23:09 +0900

    Launch a coroutine using the `viewModelScope` property in CrimeListViewModel, wrapping the initialization of the list of crimes inside the new coroutine

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -11,18 +13,20 @@ class CrimeListViewModel: ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
-        val dateFormatter = SimpleDateFormat("EEEE, MMMM dd, yyyy.", Locale.ENGLISH)
+        viewModelScope.launch {
+            val dateFormatter = SimpleDateFormat("EEEE, MMMM dd, yyyy.", Locale.ENGLISH)
 
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title = "Crime #$i",
-                date = dateFormatter.format(Date()),
-                isSolved = i % 2 == 0,
-                requiresPolice = i % 3 == 0
-            )
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = dateFormatter.format(Date()),
+                    isSolved = i % 2 == 0,
+                    requiresPolice = i % 3 == 0
+                )
 
-            crimes += crime
+                crimes += crime
+            }
         }
     }
 }
