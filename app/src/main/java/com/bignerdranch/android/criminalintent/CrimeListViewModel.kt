a87fb80b8c80823fd274b988933aebc953321dc9
Wonhui Roh <rwhui012@gmail.com>
2024-07-26 16:59:10 +0900

    Add `delay(timeMillis: Long)` call, as well as some logging calls, to initialization block of CrimeListViewModel

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,19 +1,28 @@
 package com.bignerdranch.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
+
 class CrimeListViewModel: ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init starting")
+
         viewModelScope.launch {
+            Log.d(TAG, "coroutine launched")
+            delay(5000) // suspending function
+
             val dateFormatter = SimpleDateFormat("EEEE, MMMM dd, yyyy.", Locale.ENGLISH)
 
             for (i in 0 until 100) {
@@ -27,6 +36,8 @@ class CrimeListViewModel: ViewModel() {
 
                 crimes += crime
             }
+
+            Log.d(TAG, "Loading crimes finished")
         }
     }
 }
